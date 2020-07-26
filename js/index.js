f21f4e9f75af655f83c3a51108d57ef25cbeb6ea
MisterFISHUP <68397336+MisterFISHUP@users.noreply.github.com>
2020-07-27 00:20:46 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,17 +4,3 @@ $(document).ready(function() {
         $('html,body').animate({scrollTop:$('#introduction').offset().top}, 500);});  
     }); 
 });
-
-// back to top button
-$(function() {
-	$('#to_top').click(function() { 
-		$('html,body').animate({ scrollTop:0 }, 333);
-	});
-	$(window).scroll(function() {
-		if ( $(this).scrollTop() > 300 ) {
-			$('#to_top').fadeIn(222);
-		} else {
-			$('#to_top').stop().fadeOut(222);
-		}
-	}).scroll();
-});

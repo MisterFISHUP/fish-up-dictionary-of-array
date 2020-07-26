67a7f356509ad27cf672e8965e8102757442b15a
MisterFISHUP <68397336+MisterFISHUP@users.noreply.github.com>
2020-07-27 00:18:34 +0800

    Update and rename menu_toggle.js to display.js

--- js/display.js
+++ js/display.js
@@ -7,3 +7,16 @@ function menuToggle() {
         x.className = x.className.replace(" w3-show", "");
     }
 }
+// back to top button
+$(function() {
+	$('#to_top').click(function() { 
+		$('html,body').animate({ scrollTop:0 }, 333);
+	});
+	$(window).scroll(function() {
+		if ( $(this).scrollTop() > 300 ) {
+			$('#to_top').fadeIn(222);
+		} else {
+			$('#to_top').stop().fadeOut(222);
+		}
+	}).scroll();
+});

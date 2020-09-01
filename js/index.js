2bf9cbef2d368bad9ff017a49db699f03beac65b
MisterFISHUP <68397336+MisterFISHUP@users.noreply.github.com>
2020-09-01 23:47:43 +0800

    Delete index.js

--- js/index.js
+++
@@ -1,6 +0,0 @@
-// scroll to introduction
-$(document).ready(function() {
-    $(function() { $('#to_introduction').click(function() { 
-        $('html,body').animate({scrollTop:$('#introduction').offset().top}, 500);});  
-    }); 
-});

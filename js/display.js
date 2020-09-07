ed6f84b8373257cc077b5a9fb0f9deca9d11674c
MisterFISHUP <68397336+MisterFISHUP@users.noreply.github.com>
2020-09-07 11:27:19 +0800

    Update display.js

--- js/display.js
+++ js/display.js
@@ -1,22 +1,18 @@
-// Used to toggle the menu on small screens when clicking on the menu button
-function menuToggle() {
-    var x = document.getElementById("navSmall");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else { 
-        x.className = x.className.replace(" w3-show", "");
-    }
-}
+// toggle the menu on small screens when clicking on the menu button
+$("#btnToggleNavMenu").click(function(){
+    $("#navSmall").toggleClass("w3-hide");
+});
+
 // back to top button
 $(function() {
-	$('#to_top').click(function() { 
-		$('html,body').animate({ scrollTop:0 }, 150);
-	});
-	$(window).scroll(function() {
-		if ( $(this).scrollTop() > 300 ) {
-			$('#to_top').fadeIn(222);
-		} else {
-			$('#to_top').stop().fadeOut(222);
-		}
-	}).scroll();
+    $('#to_top').click(function() { 
+        $('html,body').animate({ scrollTop:0 }, 150);
+    });
+    $(window).scroll(function() {
+        if ( $(this).scrollTop() > 300 ) {
+            $('#to_top').fadeIn(222);
+        } else {
+            $('#to_top').stop().fadeOut(222);
+        }
+    }).scroll();
 });

98fbff68c8f3f2995dd7667583e0ece6cd0bd29b
MisterFISHUP <misterfishup@gmail.com>
2021-04-21 08:50:46 +0200

    update array data

--- data-raw/array10_data.py
+++ data-raw/array10_data.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# author: Mister FISH UP
-# create data/object-array10.js from data-raw/array10.txt
+# Author: Mister FISH UP
+# https://array30.misterfishup.com/
+# Copyright © 2020-2021 FISH UP Dictionary of Array
+# Date: 2021 Jan. 21
+
+# create data/code-array10.js from data-raw/array10.txt
 
 # make sure you are in the root directory
 
 import os
-import json
+import ujson
 from pathlib import Path
 
-data_raw_folder = Path("data-raw/")
+raw_data_folder = Path("data-raw/")
 data_folder = Path("data/")
 array10 = {}
 
-with open(data_raw_folder / "array10.txt" , encoding='utf-8') as f:
+with open(raw_data_folder / "array10.txt", encoding="utf-8") as f:
     array10_raw = f.read().splitlines()
 
 for line in array10_raw:
@@ -23,8 +27,8 @@ for line in array10_raw:
     else:
         array10[char] = code
 
-with open(data_folder / "object-array10.js", "w", encoding='utf-8') as text_file:
-    text_file.write("const objectArray10 = ")
-with open(data_folder / "object-array10.js", "a", encoding='utf-8') as text_file:
-    text_file.write(json.dumps(array10, ensure_ascii=False).encode('utf8').decode())
-    text_file.write(';\n')
+with open(data_folder / "code-array10.js", "w", encoding="utf-8") as f:
+    f.write("const codeArray10=")
+with open(data_folder / "code-array10.js", "a", encoding="utf-8") as f:
+    f.write(ujson.dumps(array10, ensure_ascii=False, escape_forward_slashes=False))
+    f.write(";\n")

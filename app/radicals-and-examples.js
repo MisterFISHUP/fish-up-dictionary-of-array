99ea6e181e4e8333c4bf8167198a9ed3c76272cc
MisterFISHUP <misterfishup@gmail.com>
2023-01-21 15:15:15 +0100

    remove ecpay links

--- app/radicals-and-examples.js
+++ app/radicals-and-examples.js
@@ -9,8 +9,8 @@ const AppContent = {
   specialSCRadical: { tw: '特別歸位簡體字根', en: 'specially encoded and used only in SC', fr: 'spécialement encodé et utilisé uniquement en CS' },
   shareOn: { tw: '分享：', en: 'Share on:', fr: 'Partagez sur : ' },
   buyMeACake: {
-    tw: `如果您覺得本網站對您很有幫助，可以<a href="https://www.buymeacoffee.com/misterfishup" target="_blank" title='♫ 請我吃蛋糕 ♫'>給 FISH UP 買個蛋糕</a>來支持他！（臺灣的朋友請使用<a href="https://p.ecpay.com.tw/868A37D" target="_blank" title="支持 FISH UP 行列查碼去！">此連結</a>。）`,
-    en: `If you enjoy this website, you can support FISH UP by <a href="https://www.buymeacoffee.com/misterfishup" target="_blank" title='♫ Buy me a cake ♫'>buying him a cake</a> 🍰! (If you're from Taiwan, use <a href="https://p.ecpay.com.tw/868A37D" target="_blank" title="Support FISH UP Dictionary of Array!">this link</a> instead.)`,
-    fr: `Si vous aimez ce site, vous pouvez <a href="https://www.buymeacoffee.com/misterfishup" target="_blank" title='♫ Achetez-moi un gâteau ♫'>soutenir FISH UP en lui achetant un gâteau</a> 🍰 ! (Si vous êtes de Taïwan, utilisez plutôt <a href="https://p.ecpay.com.tw/868A37D" target="_blank" title="Soutenez Dictionnaire FISH UP de Tableau !">ce lien</a>.)`
+    tw: `如果您覺得本網站對您很有幫助，可以<a href="https://www.buymeacoffee.com/misterfishup" target="_blank" title='♫ 請我吃蛋糕 ♫'>給 FISH UP 買個蛋糕</a>來支持他！`,
+    en: `If you enjoy this website, you can support FISH UP by <a href="https://www.buymeacoffee.com/misterfishup" target="_blank" title='♫ Buy me a cake ♫'>buying him a cake</a> 🍰!`,
+    fr: `Si vous aimez ce site, vous pouvez <a href="https://www.buymeacoffee.com/misterfishup" target="_blank" title='♫ Achetez-moi un gâteau ♫'>soutenir FISH UP en lui achetant un gâteau</a> 🍰 !`
   }
 };
